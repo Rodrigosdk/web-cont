a01ab58fed7cae978187712c9cab654df168aa51
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-10-26 19:24:23 -0300

    create model vihicle

---
+++ src/main/java/com/unifip/webcont/model/VehicleModel.java
@@ -0,0 +1,13 @@
+package com.unifip.webcont.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class VehicleModel {
+    private Long code;
+    private String brand;
+    private String name;
+}
