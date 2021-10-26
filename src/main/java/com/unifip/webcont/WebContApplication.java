b0ab5c7ca0596fee288585a37822654d9fbbf878
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-10-26 18:40:36 -0300

    Initialize project

---
+++ src/main/java/com/unifip/webcont/WebContApplication.java
@@ -0,0 +1,13 @@
+package com.unifip.webcont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebContApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebContApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifip/webcont/WebContApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifip.webcont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebContApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
