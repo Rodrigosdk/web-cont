4d32cf65fea2564314f52e93039f24b2a3d69781
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-10-26 20:22:11 -0300

    update vehicle repository

--- src/main/java/com/unifip/webcont/repositorys/VehicleRepository.java
+++ src/main/java/com/unifip/webcont/repositorys/VehicleRepository.java
@@ -6,15 +6,13 @@ import java.util.Map;
 import com.unifip.webcont.model.VehicleModel;
 
 public class VehicleRepository {
+    private Map<Long, VehicleModel> vehiclesDB = new HashMap<>();
+    
     public Map<Long, VehicleModel>  searchVehicle() {
-        Map<Long, VehicleModel> vehiclesDB = new HashMap<>();
-        VehicleModel vehicleModel = new VehicleModel(100L, "FIAT", "Uno");
-        vehiclesDB.put(vehicleModel.getCode(), vehicleModel);
         return vehiclesDB;   
     }
 
     public Map<Long, VehicleModel>  create(VehicleModel vehicle) {
-        Map<Long, VehicleModel> vehiclesDB = new HashMap<>();
         vehiclesDB.put(vehicle.getCode(), vehicle);
         return vehiclesDB;   
     }
