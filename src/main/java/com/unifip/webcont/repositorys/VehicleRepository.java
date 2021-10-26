f2e6cfceabd96e8c0ddf01bd1b233a269b7d721e
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-10-26 20:02:39 -0300

    create vehicle repository

---
+++ src/main/java/com/unifip/webcont/repositorys/VehicleRepository.java
@@ -0,0 +1,15 @@
+package com.unifip.webcont.repositorys;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.unifip.webcont.model.VehicleModel;
+
+public class VehicleRepository {
+    public Map<Long, VehicleModel>  searchVehicle() {
+        Map<Long, VehicleModel> vehiclesDB = new HashMap<>();
+        VehicleModel vehicleModel = new VehicleModel(100L, "FIAT", "Uno");
+        vehiclesDB.put(vehicleModel.getCode(), vehicleModel);
+        return vehiclesDB;   
+    }
+}
